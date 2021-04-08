7cee4951e87a5b8ec49333da5ed0cfcd3e8abeca
= <=>
2021-04-08 22:02:17 +0700

    the first dev

---
+++ NotesSharingSite/notes/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request, "about.html")
+
+def index(request):
+    return render(request, "index.html")
+
+def contact(request):
+    return  render(request,'contact.html')
+
+def login(request):
+    return  render(request,'login.html')
+
+def login_admin(request):
+    return  render(request,'login_admin.html')
